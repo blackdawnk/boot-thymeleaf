7a2452470fd7be9a3415a059bc7f6ee68ad5dd86
HWJ <blackdawnk@gmail.com>
2019-05-30 14:27:41 +0900

    kanox 파일 임포트

--- src/main/java/idu/cs/BootThymeleafApplication.java
+++ src/main/java/idu/cs/BootThymeleafApplication.java
@@ -1,9 +1,12 @@
 package idu.cs;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class BootThymeleafApplication {
 
 	public static void main(String[] args) {
